99b95263b610526e125603a39aa5975360719b7a
Oleg <helgi.mail.box@gmail.com>
2015-08-11 23:56:11 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,111 @@
+var gulp = require('gulp'),
+    jade = require('gulp-jade'),
+    browserSync = require('browser-sync'),
+    watch = require('gulp-watch'),
+    reload = browserSync.reload,
+    sourcemaps = require('gulp-sourcemaps'),
+
+    minifyCss = require('gulp-minify-css'),
+    cssbeautify = require('gulp-cssbeautify'),
+    concatCss = require('gulp-concat-css'),
+    uncss = require('gulp-uncss'),
+    csso = require('gulp-csso'),
+
+
+
+    path = {
+        src: {
+            jade: 'src/*.jade',
+            css: 'src/**/*.css',
+            js: 'src/js',
+            img: 'src/img',
+            fonts: 'src/fonts'
+        },
+
+        watch: {
+            jade: 'src/**/*.jade',
+            css: 'src/**/*.css',
+            js: 'src/js',
+            img: 'src/img',
+            fonts: 'src/fonts'
+        },
+
+        dev: {
+            html: './dev',
+            css: 'dev/css',
+            js: 'dev/js',
+            img: 'dev/img',
+            fonts: 'dev/fonts'
+        }
+
+
+    },
+
+    config = {
+        server: {
+            baseDir: "./dev"
+        },
+        tunnel: true,
+        host: 'localhost',
+        port: 3000,
+        logPrefix: "dev"
+    };
+
+
+//jade
+
+gulp.task('jade', function() {
+    gulp.src(path.src.jade)
+        .pipe(jade({pretty: true}))
+        .pipe(gulp.dest(path.dev.html))
+        .pipe(reload({stream: true}));
+});
+
+//css
+
+gulp.task('css', function () {
+    gulp.src(path.src.css)
+        .pipe(sourcemaps.init())
+        .pipe(concatCss('style.css'))
+        //.pipe(uncss({html: ['dev/**/*.html']}))
+        .pipe(minifyCss({
+            keepSpecialComments: '0'
+        }))
+        .pipe(cssbeautify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dev/'))
+        .pipe(reload({stream: true}));
+});
+
+//js
+
+//gulp.task('js', function () {
+//    gulp.src(path.src.js)
+//});
+
+//dev
+
+gulp.task('dev', ['jade', 'css']);
+
+//browser-sync
+
+gulp.task('server', function() {
+    browserSync(config);
+});
+
+//watch
+
+gulp.task('watch', function() {
+    watch(path.watch.jade, function(event, cb) {
+        gulp.start('jade');
+    });
+
+    watch(path.watch.css, function(event, cb) {
+        gulp.start('css');
+    });
+});
+
+
+//default
+
+gulp.task('default', ['dev', 'server', 'watch']);
